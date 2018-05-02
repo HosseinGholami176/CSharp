0ff7926ac03ab72b6e560bc8b635f6b2f6747ead
HosseinGholami051 <Hosseingholami176@gmail.com>
2018-05-02 19:03:45 +0430

    تغییر form 1 به frmmain

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -19,7 +19,7 @@ namespace CSharp
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMain());
         }
     }
 }
